630d528150b7d717dd4ac432a92fd40a5cd546a3
0BooM <149960425+0BooM@users.noreply.github.com>
2024-07-23 22:03:19 +0200

    Setup environment
    Add modules like dev-server, css-loader, style-loader, html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/*.html"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./src/index.html",
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
