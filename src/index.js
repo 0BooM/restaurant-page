a6cb7cb0fd837d9b1ea0e965098e027e0ef2d29f
0BooM <149960425+0BooM@users.noreply.github.com>
2024-07-24 20:36:56 +0200

    Import all modules and add button event listeners
    Imported all modules and created button listeners. Now when clicking on a button, appropriate site appears. Before it, the HTML of div.content is being cleared

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import loadHomePage from "./modules/home.js";
 import loadMenuPage from "./modules/menu.js";
+import loadAboutPage from "./modules/about.js";
+
+loadHomePage();
+
+function btnsEventListeners() {
+  const content = document.querySelector("#content");
+
+  const homeBtn = document.querySelector("#home");
+  const menuBtn = document.querySelector("#menu");
+  const aboutBtn = document.querySelector("#about");
+
+  homeBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadHomePage();
+  });
+
+  menuBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadMenuPage();
+  });
+
+  aboutBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadAboutPage();
+  });
+}
+
+btnsEventListeners();
